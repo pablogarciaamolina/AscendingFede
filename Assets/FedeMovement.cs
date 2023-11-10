9644ab0ec27e9fb22f53d9556c0c9273f483c4a9
DESKTOP-ABB8PBE\pablo <202110501@alu.comillas.edu>
2023-11-10 18:58:20 +0100

    Working on basic movement and change of  plane of movement

--- Assets/FedeMovement.cs
+++ Assets/FedeMovement.cs
@@ -1,42 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class FedeMovement : MonoBehaviour
 {
+    [Header("Parameters")]
+    [SerializeField] float movingForce = 35f;
+    [SerializeField] float jumpForce = 100f;
 
-    [SerializeField] private Rigidbody rigidBody;
+    // Constants
+    private const float rotationAmount = -90f;
 
+    // Physiscs elements
+    private Rigidbody rb;
+
+    // Moving Direction
+    private List<Vector3> Directions = new List<Vector3>() { new Vector3(1f, 0f, 0f), new Vector3(0f, 0f, 1f), new Vector3(-1f, 0f, 0f), new Vector3(0f, 0f, -1f) };
+    private int directionIndex = 0;
+    private Vector3 direction;
+
+    // Moving Input
+    private float moveHorizontal;
+    private bool doJump;
+    private bool rotatePositive;
+    private bool rotateNegative;
+
+    //Movement variables
+    private Vector3 movingVector;
+    private bool isJumping;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody>();
+        direction = Directions[directionIndex];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D)) 
-        {
-            MoveDirection(Vector3.right);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            MoveDirection(Vector3.left);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            MoveDirection(Vector3.up);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            MoveDirection(Vector3.down);
-        }
+        // Get Input
+        moveHorizontal = Input.GetAxis("Horizontal");
+        doJump = Input.GetKey(KeyCode.Space);
+        rotatePositive = Input.GetKeyDown(KeyCode.G);
+        rotateNegative = Input.GetKeyDown(KeyCode.F);
+
+        // Check grounded
+        // ...
+
+        // Prepare Direction
+        if (rotatePositive)  { ChangeDirection(1); }
+        if (rotateNegative) { ChangeDirection(-1); }
+
+        // Prepare movement
+        movingVector = PrepareStraightMove();
+        movingVector += PrepareJump();
+
+        // Do movement
+        ApplyForce(movingVector);
+
     }
 
-    private void MoveDirection(Vector3 direction)
+    // Quick implementation of modulo operation
+    private int Mod(int k, int n) { return ((k %= n) < 0) ? k + n : k; }
+    
+    private void ApplyForce(Vector3 direction)
     {
-        transform.position += direction * Time.deltaTime;
+        rb.AddForce(direction);
+    }
+
+    private Vector3 PrepareStraightMove()
+    {
+        return direction * moveHorizontal * movingForce;
+    }
+
+    private Vector3 PrepareJump()
+    {
+        Vector3 jumpVector = Vector3.zero;
+        if (doJump && !isJumping)
+        {
+            jumpVector += Vector3.up * jumpForce;
+            doJump = false;
+            isJumping = true;
+        }
+
+        return jumpVector;
+    }
+    private void ChangeDirection(int way)
+    {
+        directionIndex = Mod(directionIndex + way, Directions.Count);
+        direction = Directions[directionIndex];
+        RotateByAomunt(rotationAmount*way);
+    }
+    private void RotateByAomunt(float amount)
+    {
+        // Rotate the object around its up axis by the specified amount
+        transform.Rotate(Vector3.up, amount);
     }
 }
